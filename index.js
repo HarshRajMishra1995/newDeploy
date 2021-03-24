57411bac75c1263f17e2116ba69a124b997a3343
harshrajmishra1995 <harshrajmishra.1995@gmail.com>
2021-03-24 12:24:57 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const express=require('express');
+const app=express();
+const port=process.env.PORT||3000;
+
+app.get('/',(req,res)=>{
+    res.send("App deployed")
+})
+
+
+app.listen(port,()=>{
+    console.log("app is running at port 3000")
+})
